d7ee098493556c9a84b6e3a664fbd9b853a81e6a
Aditya kumar <Adityakumar14022002@gmail.com>
2024-02-09 02:00:28 +0530

    done these comp

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from "react";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const LandingPage = () => {
   return (
     <div>
-       <div className='w-full h-screen bg-zinc-900 pt-1'>
-         <div className="testStructue mt-56 px-20">
-            {[" WE CREATE" , "EYE-OPENING" ,"PRESENTATIONS"].map((item , idx)=>(
-                           <div className='masker'>
-                           <h1 className='tracking-tighter leading-[6vw] uppercase text-[7.5vw] font-["Founders Grotesk"] font-medium'>
-                               {item}
-                           </h1>
-                       </div>
-            ))}
-         </div>
-         <div className="border-t-[1px] border-zinc-800 mt-32"></div>
-       </div>
+      <div className="w-full h-screen bg-zinc-900 pt-1">
+        <div className="testStructue mt-56 px-20">
+          {[" WE CREATE", "EYE-OPENING", "PRESENTATIONS"].map((item, idx) => (
+            <div className="masker">
+              <div className="w-fit flex items-center">
+                {idx ==1 && (<div className="rounded-md mr-2 relative -top-[1.7vw] w-[9vw] h-[6vw] bg-green-500"></div>)}
+              <h1 className='leading-[7.2vw] uppercase text-[9.5vw] font-["Founders_Grotesk_X-Condensed"] font-semibold'>
+                {item}
+              </h1>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="border-t-[1px] border-zinc-800 mt-28 flex justify-between items-center py-5 px-20">
+          {[
+            "For public and private companies",
+            "From the first pitch to IPO",
+          ].map((item, idx) => (
+            <p className="text-md tracking-tight leading-none font-light">
+              {item}
+            </p>
+          ))}
+          <div className="start flex justify-center items-center">
+            <div className="capitalize text-sm px-5 py-2 border-[2px] border-x-zinc-600 rounded-full">
+              {" "}
+              START THE PROJECT
+            </div>
+            <div className="text-white mx-2 w-3 h-3 rounded-full border-[2px] border-x-zinc-600 p-5 flex items-center justify-center">
+              <span className="rotate-[245deg]">
+                <FaArrowDownLong />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
-
+export default LandingPage;
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Marquee = () => {
+  return (
+    <div className="rounded-tl-3xl rounded-tr-3xl w-full py-10 bg-[#1e4e43]">
+      <div className="flex whitespace-nowrap gap-40 overflow-hidden text  border-t-2 border-b-2 border-zinc-300">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity , duration: 5 }}
+          className='text-[18vw] leading-none font-["Founders_Grotesk_X-Condensed"] font-semibold uppercase pt-10  -mb-12'
+        >
+          we are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity  , duration: 5}}
+          className='text-[18vw] leading-none font-["Founders_Grotesk_X-Condensed"] font-semibold uppercase pt-10 -mb-12'
+        >
+          we are ochi
+        </motion.h1>
+      </div>
+    </div>
+  );
+};
+export default Marquee;
